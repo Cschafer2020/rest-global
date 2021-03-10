da82aef08828bb63575b288299c4a96bded7b012
Cschafer2020 <71891473+Cschafer2020@users.noreply.github.com>
2021-03-10 14:13:05 +1100

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,9 +6,25 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <SEO title="Property" />
+    <h1>GERX is a key initiative of Greening Australia. </h1>
+    <p>GERX is a marketplace where you can earn money, increase the value of your land, and restore the environment, all in one place.</p>
+  
+ <p> If your land qualifies for one of our Carbon projects, our team can restore your land while simultaneously increasing its value, and making you money from carbon credits . And, you'll be helping the environment in the process! <p>
+  <p> How it works:
+Register your land on LandPortal.com.au for free
+Get a free assessment of your land 
+Work with experts to design your restoration projects 
+Have our team implement the whole project - zero labour on your end.
+Increase the value of your property and reap the rewards 
+
+GERX requires no payment from the landholder. What's the catch? None – we are able to offer you money to restore your land because the carbon market is growing, and demand keeps increasing at a rate we can barely keep up with. <p>
+
+<p> What is the carbon market?
+Organisations buying carbon credits to voluntarily or compulsorily reduce their carbon footprints purchase carbon credits. Carbon credits are created by forest growth, so as tress grow on your restored property, you profit from the carbon market. 
+
+This is as close as you'll ever get to money growing on trees. ,P>
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
